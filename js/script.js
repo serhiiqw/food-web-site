bd2284247403c5f347ba064afed4614924e94f40
serhiiqw <sergeikhodos6@gmail.com>
2026-01-25 13:34:44 -0500

    Updated timer 1

--- js/script.js
+++ js/script.js
@@ -41,11 +41,21 @@ window.addEventListener('DOMContentLoaded', function() {
     const deadLine = '2026-01-25';
     
     function getTimeRemaining(endTime) {
-        const t = Date.parse(endTime) - Date.parse(new Date()),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
               days = Math.floor(t / (1000 * 60 * 60 *24)),
               hours = Math.floor((t / (1000 * 60 * 60) % 24)),
               minutes = Math.floor((t / 1000 / 60) % 60),
               seconds = Math.floor((t / 1000) % 60);
+        };
+
         
         return {
             'total': t,
