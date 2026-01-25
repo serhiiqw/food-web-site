641efea85e77df1bbdbfac9ee079800ffea06343
serhiiqw <sergeikhodos6@gmail.com>
2026-01-25 13:32:15 -0500

    Updated timer

--- js/script.js
+++ js/script.js
@@ -36,6 +36,59 @@ window.addEventListener('DOMContentLoaded', function() {
             });
 		}
 	});
+
+    //Timer
+    const deadLine = '2026-01-25';
+    
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 *24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        
+        return {
+            'total': t,
+            'days' : days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >=0 && num <= 10) {
+            return `0${num}`
+        } else {
+           return num;
+        }
+    }
+
+    function setTimer(selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timerInterval = setInterval(updateTimer, 1000);
+
+            updateTimer();
+
+            function updateTimer(){
+                const t = getTimeRemaining(endTime);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timerInterval);
+                }
+            }
+    }
+    
+    setTimer('.timer', deadLine);
 });
 
 
